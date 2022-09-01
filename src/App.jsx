1ac011cd629e84119e4f123c16d568bde56b01ad
Robert Uridge <856071+ruridge@users.noreply.github.com>
2022-09-01 14:31:54 +0800

    Update deps and fix css bug in App.css
    - also improve demo layout, add multi colours and resize

--- src/App.jsx
+++ src/App.jsx
@@ -5,22 +5,10 @@ import { Neon } from "./components/Neon";
 function App() {
   return (
     <div className="App">
-      {/* <Neon color={Neon.color.DEFAULT}>Neon 理想</Neon>
+      <Neon color={Neon.color.DEFAULT}>Neon 理想</Neon>
       <Neon color={Neon.color.RED}>Neon 紅</Neon>
       <Neon color={Neon.color.GREEN}>Neon 綠</Neon>
-      <Neon color={Neon.color.BLUE}>Neon 藍</Neon> */}
-      <Neon color={Neon.color.BLUE}>
-        BEDTIME{" "}
-        <sub>
-          z
-          <sup>
-            z
-            <sup>
-              z<sup>z</sup>
-            </sup>
-          </sup>
-        </sub>
-      </Neon>
+      <Neon color={Neon.color.BLUE}>Neon 藍</Neon>
     </div>
   );
 }
