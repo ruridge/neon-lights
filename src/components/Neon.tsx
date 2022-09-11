11e69a70e651cc292183fae13dbec0b8d7d8f933
Robert Uridge <856071+ruridge@users.noreply.github.com>
2022-09-11 15:41:23 +0800

    Add react to Astro

---
+++ src/components/Neon.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import clsx from "clsx";
+import "./Neon.css";
+
+const COLORS = {
+  DEFAULT: "default",
+  RED: "red",
+  GREEN: "green",
+  BLUE: "blue",
+} as const;
+
+const COLOR_MAP = {
+  [COLORS.DEFAULT]: "",
+  [COLORS.RED]: "Neon-red",
+  [COLORS.GREEN]: "Neon-green",
+  [COLORS.BLUE]: "Neon-blue",
+};
+
+type Color = typeof COLORS[keyof typeof COLORS];
+function Neon(props: { children: React.ReactNode; color: Color }) {
+  return (
+    <div className={clsx("Neon", COLOR_MAP[props.color])}>{props.children}</div>
+  );
+}
+
+export { Neon };
